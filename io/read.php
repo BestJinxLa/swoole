52fe386d8e09202272b62be5051c83586d85401f
le118 <516995619@qq.com>
2019-11-21 18:33:47 +0800

    file async

--- io/read.php
+++ io/read.php
@@ -1,9 +1,29 @@
 <?php
 /**
+ * swoole异步读取文件
  * @auth le118
  * date 2019/11/21
  */
 
-swoole_async_readfile(__DIR__."/1.txt", function($filename, $content) {
+/*
+ * 先读取文件返回true/false
+ * 再执行 start
+ * 最后执行 闭包函数
+ * */
+//$res = Swoole\Async::readfile(__DIR__."/1.txt", function($filename, $content) {
+////$res = swoole_async_readfile(__DIR__."/1.txt", function($filename, $content) {
+//    echo "$filename: $content".PHP_EOL;
+//});
+
+//swoole_async_readfile
+$res = Swoole\Async::read(__DIR__."/1.txt", function($filename, $content) {
+    /*会输出文本长度/2次*/
     echo "$filename: $content".PHP_EOL;
-});
+}, 2);
+
+
+
+var_dump($res);
+
+sleep(2);
+echo "start".PHP_EOL;
