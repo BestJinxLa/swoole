55939189dddd5fb86fc28db232c8e3502d824393
le118 <516995619@qq.com>
2019-11-21 15:22:48 +0800

    task任务

--- server/Ws.php
+++ server/Ws.php
@@ -1,6 +1,11 @@
 <?php
 /**
  * Ws 优化基础类库
+ * task使用 $ws->task($data);
+ * onTask
+ * onFinish
+ * 设置task_worker_num
+ *
  * @auth le118
  * date 2019/11/18
  */
@@ -14,9 +19,17 @@ class Ws
     public function __construct()
     {
         $this->ws = new Swoole\WebSocket\Server(self::HOST, self::PORT);
+
+        $this->ws->set([
+            'worker_num' => 2,
+            'task_worker_num' => 2,
+        ]);
+
         /*注意对象的回调函数的写法*/
         $this->ws->on('open', [$this, 'onOpen']);
         $this->ws->on('message', [$this, 'onMessage']);
+        $this->ws->on('task', [$this, 'onTask']);
+        $this->ws->on('finish', [$this, 'onFinish']);
         $this->ws->on('close', [$this, 'onClose']);
         $this->ws->start();
     }
@@ -43,8 +56,42 @@ class Ws
     public function onMessage($ws, $frame)
     {
         echo "ser-push-message:{$frame->data} \n";
-        $ws->push($frame->fd, 'server-push:' . date('Y-m-d H:i:s'));
+        $data = [
+            'task_id' => 1,
+            'task_content' => 'first task',
+        ];
+        $ws->task($data);
+        $ws->push($frame->fd, 'server-push-success:' . date('Y-m-d H:i:s'));
     }
+
+    /**
+     * 监听任务事件
+     * 执行完之后会触发onFinish事件
+     * @param $ws
+     * @param $task_id，任务ID，由swoole扩展内自动生成，用于区分不同的任务
+     * $task_id和$src_worker_id组合起来才是全局唯一的，不同的worker进程投递的任务ID可能会有相同
+     * @param $src_worker_id，来自于哪个worker进程
+     * @param $data , 任务的内容
+     */
+    public function onTask($ws, $task_id, $src_worker_i, $data)
+    {
+        print_r($data);
+        sleep(10);
+        return "task finished";
+    }
+
+    /**
+     * 监听ws 任务完成事件
+     * @param $ws
+     * @param $task_id
+     * @param $data，这里的data是onTask返回的数据
+     */
+    public function onFinish($ws, $task_id, $data)
+    {
+        echo "taskID:{$task_id} \n";
+        echo "finish-data-success:{$data} \n";
+    }
+
 
     /**
      * 监听ws的close事件
